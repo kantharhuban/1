5bb9af308726225f9c0e3579c8ab020e71a3c76e
r.kantharhuban <46379603+kantharhuban@users.noreply.github.com>
2019-01-04 21:35:31 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class one
+	{
+		public static void main(String args[])
+			{
+				Scanner s=new Scanner(System.in);
+				if(s>0)
+				{
+					System.out.println("positive");
+				}
+				else if(s<0)
+				{
+					System.out.println("negative");
+				}
+				else if(s==0)
+				{
+					System.out.println("zero");
+	
+				}
+			}
+	}
